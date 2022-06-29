31903acf3d88df12521efb861f516c4ae8d432d3
VikkiVuk <viktorblazaric2@gmail.com>
2022-06-30 01:13:22 +0200

    added structures

--- src/main/java/xyz/vikkivuk/chaosmod/world/features/AmongUSpORTALFeature.java
+++ src/main/java/xyz/vikkivuk/chaosmod/world/features/AmongUSpORTALFeature.java
@@ -25,15 +25,15 @@ import net.minecraft.core.BlockPos;
 import java.util.Set;
 import java.util.List;
 
-public class AmongUSppFeature extends Feature<NoneFeatureConfiguration> {
-	public static AmongUSppFeature FEATURE = null;
+public class AmongUSpORTALFeature extends Feature<NoneFeatureConfiguration> {
+	public static AmongUSpORTALFeature FEATURE = null;
 	public static Holder<ConfiguredFeature<NoneFeatureConfiguration, ?>> CONFIGURED_FEATURE = null;
 	public static Holder<PlacedFeature> PLACED_FEATURE = null;
 
 	public static Feature<?> feature() {
-		FEATURE = new AmongUSppFeature();
-		CONFIGURED_FEATURE = FeatureUtils.register("chaosmod:among_u_spp", FEATURE, FeatureConfiguration.NONE);
-		PLACED_FEATURE = PlacementUtils.register("chaosmod:among_u_spp", CONFIGURED_FEATURE, List.of());
+		FEATURE = new AmongUSpORTALFeature();
+		CONFIGURED_FEATURE = FeatureUtils.register("chaosmod:among_u_sp_ortal", FEATURE, FeatureConfiguration.NONE);
+		PLACED_FEATURE = PlacementUtils.register("chaosmod:among_u_sp_ortal", CONFIGURED_FEATURE, List.of());
 		return FEATURE;
 	}
 
@@ -48,7 +48,7 @@ public class AmongUSppFeature extends Feature<NoneFeatureConfiguration> {
 			ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation("chaosmod:sus")));
 	private StructureTemplate template = null;
 
-	public AmongUSppFeature() {
+	public AmongUSpORTALFeature() {
 		super(NoneFeatureConfiguration.CODEC);
 	}
 
@@ -57,20 +57,20 @@ public class AmongUSppFeature extends Feature<NoneFeatureConfiguration> {
 		if (!generate_dimensions.contains(context.level().getLevel().dimension()))
 			return false;
 		if (template == null)
-			template = context.level().getLevel().getStructureManager().getOrCreate(new ResourceLocation("chaosmod", "amongus"));
+			template = context.level().getLevel().getStructureManager().getOrCreate(new ResourceLocation("chaosmod", "amongusportal"));
 		if (template == null)
 			return false;
 		boolean anyPlaced = false;
-		if ((context.random().nextInt(1000000) + 1) <= 100000) {
+		if ((context.random().nextInt(1000000) + 1) <= 5000) {
 			int count = context.random().nextInt(1) + 1;
 			for (int a = 0; a < count; a++) {
 				int i = context.origin().getX() + context.random().nextInt(16);
 				int k = context.origin().getZ() + context.random().nextInt(16);
 				int j = context.level().getHeight(Heightmap.Types.OCEAN_FLOOR_WG, i, k) - 1;
-				BlockPos spawnTo = new BlockPos(i + 0, j + 0, k + 0);
+				BlockPos spawnTo = new BlockPos(i + 0, j + 5, k + 0);
 				if (template.placeInWorld(
 						context.level(), spawnTo, spawnTo, new StructurePlaceSettings().setMirror(Mirror.NONE).setRotation(Rotation.NONE)
-								.setRandom(context.random()).addProcessor(BlockIgnoreProcessor.STRUCTURE_AND_AIR).setIgnoreEntities(false),
+								.setRandom(context.random()).addProcessor(BlockIgnoreProcessor.STRUCTURE_BLOCK).setIgnoreEntities(false),
 						context.random(), 2)) {
 					anyPlaced = true;
 				}
--- src/main/java/xyz/vikkivuk/chaosmod/world/features/JukeBoxCaveFeature.java
+++ src/main/java/xyz/vikkivuk/chaosmod/world/features/JukeBoxCaveFeature.java
@@ -18,22 +18,21 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.features.FeatureUtils;
-import net.minecraft.core.Registry;
 import net.minecraft.core.Holder;
 import net.minecraft.core.BlockPos;
 
 import java.util.Set;
 import java.util.List;
 
-public class AmongUSppFeature extends Feature<NoneFeatureConfiguration> {
-	public static AmongUSppFeature FEATURE = null;
+public class JukeBoxCaveFeature extends Feature<NoneFeatureConfiguration> {
+	public static JukeBoxCaveFeature FEATURE = null;
 	public static Holder<ConfiguredFeature<NoneFeatureConfiguration, ?>> CONFIGURED_FEATURE = null;
 	public static Holder<PlacedFeature> PLACED_FEATURE = null;
 
 	public static Feature<?> feature() {
-		FEATURE = new AmongUSppFeature();
-		CONFIGURED_FEATURE = FeatureUtils.register("chaosmod:among_u_spp", FEATURE, FeatureConfiguration.NONE);
-		PLACED_FEATURE = PlacementUtils.register("chaosmod:among_u_spp", CONFIGURED_FEATURE, List.of());
+		FEATURE = new JukeBoxCaveFeature();
+		CONFIGURED_FEATURE = FeatureUtils.register("chaosmod:juke_box_cave", FEATURE, FeatureConfiguration.NONE);
+		PLACED_FEATURE = PlacementUtils.register("chaosmod:juke_box_cave", CONFIGURED_FEATURE, List.of());
 		return FEATURE;
 	}
 
@@ -42,13 +41,10 @@ public class AmongUSppFeature extends Feature<NoneFeatureConfiguration> {
 	}
 
 	public static final Set<ResourceLocation> GENERATE_BIOMES = null;
-	private final Set<ResourceKey<Level>> generate_dimensions = Set.of(Level.OVERWORLD, Level.NETHER, Level.END,
-			ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation("chaosmod:peemension")),
-			ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation("chaosmod:wood_dimension")),
-			ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation("chaosmod:sus")));
+	private final Set<ResourceKey<Level>> generate_dimensions = Set.of(Level.OVERWORLD);
 	private StructureTemplate template = null;
 
-	public AmongUSppFeature() {
+	public JukeBoxCaveFeature() {
 		super(NoneFeatureConfiguration.CODEC);
 	}
 
@@ -57,20 +53,21 @@ public class AmongUSppFeature extends Feature<NoneFeatureConfiguration> {
 		if (!generate_dimensions.contains(context.level().getLevel().dimension()))
 			return false;
 		if (template == null)
-			template = context.level().getLevel().getStructureManager().getOrCreate(new ResourceLocation("chaosmod", "amongus"));
+			template = context.level().getLevel().getStructureManager().getOrCreate(new ResourceLocation("chaosmod", "jukebox_cave"));
 		if (template == null)
 			return false;
 		boolean anyPlaced = false;
-		if ((context.random().nextInt(1000000) + 1) <= 100000) {
+		if ((context.random().nextInt(1000000) + 1) <= 50000) {
 			int count = context.random().nextInt(1) + 1;
 			for (int a = 0; a < count; a++) {
 				int i = context.origin().getX() + context.random().nextInt(16);
 				int k = context.origin().getZ() + context.random().nextInt(16);
 				int j = context.level().getHeight(Heightmap.Types.OCEAN_FLOOR_WG, i, k) - 1;
 				BlockPos spawnTo = new BlockPos(i + 0, j + 0, k + 0);
-				if (template.placeInWorld(
-						context.level(), spawnTo, spawnTo, new StructurePlaceSettings().setMirror(Mirror.NONE).setRotation(Rotation.NONE)
-								.setRandom(context.random()).addProcessor(BlockIgnoreProcessor.STRUCTURE_AND_AIR).setIgnoreEntities(false),
+				if (template.placeInWorld(context.level(), spawnTo, spawnTo,
+						new StructurePlaceSettings().setMirror(Mirror.values()[context.random().nextInt(2)])
+								.setRotation(Rotation.values()[context.random().nextInt(3)]).setRandom(context.random())
+								.addProcessor(BlockIgnoreProcessor.AIR).setIgnoreEntities(false),
 						context.random(), 2)) {
 					anyPlaced = true;
 				}
