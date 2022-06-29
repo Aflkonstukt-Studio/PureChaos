80ecb9cbc2606676f49d14ed0d1ffe918db0ed86
XMasterWoo <petar@Xbookpromax2021>
2022-06-29 23:47:11 +0200

    i suck his dick with a smile ony

--- src/main/java/xyz/vikkivuk/chaosmod/world/dimension/SusDimension.java
+++ src/main/java/xyz/vikkivuk/chaosmod/world/dimension/SusDimension.java
@@ -1,24 +1,42 @@
 
 package xyz.vikkivuk.chaosmod.world.dimension;
 
+import xyz.vikkivuk.chaosmod.init.ChaosmodModBlocks;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.levelgen.carver.WorldCarver;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+
+import java.util.Set;
+import java.util.HashSet;
+
+import com.google.common.collect.ImmutableSet;
+
 @Mod.EventBusSubscriber
 public class SusDimension {
-
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 	public static class Fixers {
-
 		@SubscribeEvent
 		public static void registerFillerBlocks(FMLCommonSetupEvent event) {
 			Set<Block> replaceableBlocks = new HashSet<>();
 			replaceableBlocks.add(ChaosmodModBlocks.SUS_BLOCK.get());
-
+			replaceableBlocks.add(Blocks.GRASS_BLOCK);
+			replaceableBlocks.add(ChaosmodModBlocks.SUS_BLOCK.get());
 			replaceableBlocks.add(Blocks.NETHERRACK);
 			replaceableBlocks.add(Blocks.NETHERRACK);
-
 			event.enqueueWork(() -> {
 				WorldCarver.CAVE.replaceableBlocks = new ImmutableSet.Builder<Block>().addAll(WorldCarver.CAVE.replaceableBlocks)
 						.addAll(replaceableBlocks).build();
-
 				WorldCarver.CANYON.replaceableBlocks = new ImmutableSet.Builder<Block>().addAll(WorldCarver.CANYON.replaceableBlocks)
 						.addAll(replaceableBlocks).build();
 			});
@@ -28,7 +46,6 @@ public class SusDimension {
 		@OnlyIn(Dist.CLIENT)
 		public static void registerDimensionSpecialEffects(FMLClientSetupEvent event) {
 			DimensionSpecialEffects customEffect = new DimensionSpecialEffects(128, true, DimensionSpecialEffects.SkyType.NORMAL, false, false) {
-
 				@Override
 				public Vec3 getBrightnessDependentFogColor(Vec3 color, float sunHeight) {
 					return new Vec3(1, 0.2, 1);
@@ -38,12 +55,8 @@ public class SusDimension {
 				public boolean isFoggyAt(int x, int y) {
 					return true;
 				}
-
 			};
-
 			event.enqueueWork(() -> DimensionSpecialEffects.EFFECTS.put(new ResourceLocation("chaosmod:sus"), customEffect));
 		}
-
 	}
-
 }
--- src/main/java/xyz/vikkivuk/chaosmod/world/features/treedecorators/SususLeaveDecorator.java
+++ src/main/java/xyz/vikkivuk/chaosmod/world/features/treedecorators/SususLeaveDecorator.java
@@ -1,12 +1,24 @@
 package xyz.vikkivuk.chaosmod.world.features.treedecorators;
 
+import xyz.vikkivuk.chaosmod.init.ChaosmodModBlocks;
+
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecoratorType;
+import net.minecraft.world.level.levelgen.feature.treedecorators.LeaveVineDecorator;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.LevelSimulatedReader;
+import net.minecraft.core.BlockPos;
+
+import java.util.function.BiConsumer;
+import java.util.Random;
+import java.util.List;
+
 public class SususLeaveDecorator extends LeaveVineDecorator {
-
 	public static final SususLeaveDecorator INSTANCE = new SususLeaveDecorator();
-
 	public static com.mojang.serialization.Codec<LeaveVineDecorator> codec;
 	public static TreeDecoratorType<?> tdt;
-
 	static {
 		codec = com.mojang.serialization.Codec.unit(() -> INSTANCE);
 		tdt = new TreeDecoratorType<>(codec);
@@ -29,7 +41,6 @@ public class SususLeaveDecorator extends LeaveVineDecorator {
 					addVine(level, bp, biConsumer);
 				}
 			}
-
 		});
 	}
 
@@ -40,7 +51,5 @@ public class SususLeaveDecorator extends LeaveVineDecorator {
 			biConsumer.accept(blockpos, ChaosmodModBlocks.SA_DS.get().defaultBlockState());
 			blockpos = blockpos.below();
 		}
-
 	}
-
 }
