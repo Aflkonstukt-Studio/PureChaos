e8d426043ff2aeb4308cb18a694f0f5d21acd913
Josip <Josip@DESKTOP-TD73I9K.home>
2022-06-30 01:28:48 +0200

    MCreator Merge commit 'f9fa49d476ed5eaedd0d1c855483383213809309' into f7e85cf8a53436098d1d50352d09c8237b83c8f7

--- src/main/java/xyz/vikkivuk/chaosmod/init/ChaosmodModFeatures.java
+++ src/main/java/xyz/vikkivuk/chaosmod/init/ChaosmodModFeatures.java
@@ -12,12 +12,7 @@ import xyz.vikkivuk.chaosmod.world.features.ores.DeathFeature;
 import xyz.vikkivuk.chaosmod.world.features.lakes.StaravodaFeature;
 import xyz.vikkivuk.chaosmod.world.features.lakes.PeeFeature;
 import xyz.vikkivuk.chaosmod.world.features.lakes.CumFeature;
-import xyz.vikkivuk.chaosmod.world.features.WoodRuinedPortalFeature;
-import xyz.vikkivuk.chaosmod.world.features.PeePoolFeature;
-import xyz.vikkivuk.chaosmod.world.features.JukeBoxCaveFeature;
-import xyz.vikkivuk.chaosmod.world.features.AmongUsDungeonFeature;
 import xyz.vikkivuk.chaosmod.world.features.AmongUSppFeature;
-import xyz.vikkivuk.chaosmod.world.features.AmongUSpORTALFeature;
 import xyz.vikkivuk.chaosmod.ChaosmodMod;
 
 import net.minecraftforge.registries.RegistryObject;
@@ -56,23 +51,10 @@ public class ChaosmodModFeatures {
 			new FeatureRegistration(GenerationStep.Decoration.UNDERGROUND_ORES, DeathFeature.GENERATE_BIOMES, DeathFeature::placedFeature));
 	public static final RegistryObject<Feature<?>> CUM = register("cum", CumFeature::feature,
 			new FeatureRegistration(GenerationStep.Decoration.LAKES, CumFeature.GENERATE_BIOMES, CumFeature::placedFeature));
-	public static final RegistryObject<Feature<?>> AMONG_U_SPP = register("among_u_spp", AmongUSppFeature::feature,
-			new FeatureRegistration(GenerationStep.Decoration.SURFACE_STRUCTURES, AmongUSppFeature.GENERATE_BIOMES, AmongUSppFeature::placedFeature));
 	public static final RegistryObject<Feature<?>> STARAVODA = register("staravoda", StaravodaFeature::feature,
 			new FeatureRegistration(GenerationStep.Decoration.LAKES, StaravodaFeature.GENERATE_BIOMES, StaravodaFeature::placedFeature));
-	public static final RegistryObject<Feature<?>> AMONG_U_SP_ORTAL = register("among_u_sp_ortal", AmongUSpORTALFeature::feature,
-			new FeatureRegistration(GenerationStep.Decoration.SURFACE_STRUCTURES, AmongUSpORTALFeature.GENERATE_BIOMES,
-					AmongUSpORTALFeature::placedFeature));
-	public static final RegistryObject<Feature<?>> AMONG_US_DUNGEON = register("among_us_dungeon", AmongUsDungeonFeature::feature,
-			new FeatureRegistration(GenerationStep.Decoration.SURFACE_STRUCTURES, AmongUsDungeonFeature.GENERATE_BIOMES,
-					AmongUsDungeonFeature::placedFeature));
-	public static final RegistryObject<Feature<?>> JUKE_BOX_CAVE = register("juke_box_cave", JukeBoxCaveFeature::feature, new FeatureRegistration(
-			GenerationStep.Decoration.SURFACE_STRUCTURES, JukeBoxCaveFeature.GENERATE_BIOMES, JukeBoxCaveFeature::placedFeature));
-	public static final RegistryObject<Feature<?>> PEE_POOL = register("pee_pool", PeePoolFeature::feature,
-			new FeatureRegistration(GenerationStep.Decoration.SURFACE_STRUCTURES, PeePoolFeature.GENERATE_BIOMES, PeePoolFeature::placedFeature));
-	public static final RegistryObject<Feature<?>> WOOD_RUINED_PORTAL = register("wood_ruined_portal", WoodRuinedPortalFeature::feature,
-			new FeatureRegistration(GenerationStep.Decoration.SURFACE_STRUCTURES, WoodRuinedPortalFeature.GENERATE_BIOMES,
-					WoodRuinedPortalFeature::placedFeature));
+	public static final RegistryObject<Feature<?>> AMONG_U_SPP = register("among_u_spp", AmongUSppFeature::feature,
+			new FeatureRegistration(GenerationStep.Decoration.SURFACE_STRUCTURES, AmongUSppFeature.GENERATE_BIOMES, AmongUSppFeature::placedFeature));
 
 	private static RegistryObject<Feature<?>> register(String registryname, Supplier<Feature<?>> feature, FeatureRegistration featureRegistration) {
 		FEATURE_REGISTRATIONS.add(featureRegistration);
