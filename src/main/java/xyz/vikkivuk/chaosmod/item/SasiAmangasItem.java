e033264e259f2030e862b1ff4dac6c102cdd02f1
XMasterWoo <petar@Xbookpromax2021>
2022-06-30 15:18:14 +0200

    josip disk

---
+++ src/main/java/xyz/vikkivuk/chaosmod/item/SasiAmangasItem.java
@@ -0,0 +1,40 @@
+
+package xyz.vikkivuk.chaosmod.item;
+
+import xyz.vikkivuk.chaosmod.init.ChaosmodModTabs;
+import xyz.vikkivuk.chaosmod.init.ChaosmodModSounds;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.RecordItem;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
+
+public class SasiAmangasItem extends RecordItem {
+	public SasiAmangasItem() {
+		super(0, ChaosmodModSounds.REGISTRY.get(new ResourceLocation("chaosmod:jusif")),
+				new Item.Properties().tab(ChaosmodModTabs.TAB_CHAOSTAB).stacksTo(1).rarity(Rarity.RARE));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public boolean isFoil(ItemStack itemstack) {
+		return true;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+		list.add(new TextComponent("josip kako drka u mikrofon"));
+		list.add(new TextComponent("josip je nacist"));
+	}
+}
