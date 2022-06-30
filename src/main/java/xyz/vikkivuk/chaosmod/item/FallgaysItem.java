e0620a217d58e9d9362e58180d7b7d561d0c4624
XMasterWoo <petar@Xbookpromax2021>
2022-06-30 16:06:32 +0200

    josip

--- src/main/java/xyz/vikkivuk/chaosmod/item/FallgaysItem.java
+++ src/main/java/xyz/vikkivuk/chaosmod/item/FallgaysItem.java
@@ -1,13 +1,17 @@
 
 package xyz.vikkivuk.chaosmod.item;
 
-import net.minecraft.network.chat.Component;
+import xyz.vikkivuk.chaosmod.init.ChaosmodModTabs;
+import xyz.vikkivuk.chaosmod.init.ChaosmodModSounds;
+
+import net.minecraft.world.item.RecordItem;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+import net.minecraft.resources.ResourceLocation;
 
 public class FallgaysItem extends RecordItem {
-
 	public FallgaysItem() {
 		super(0, ChaosmodModSounds.REGISTRY.get(new ResourceLocation("chaosmod:fallguyswooh")),
 				new Item.Properties().tab(ChaosmodModTabs.TAB_CHAOSTAB).stacksTo(1).rarity(Rarity.RARE));
 	}
-
 }
