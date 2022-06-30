9eaccd3dd2fa6c4e860ca4183677e2d9bb1e7067
VikkiVuk <viktorblazaric2@gmail.com>
2022-06-30 17:46:03 +0200

    Fixed Brown (again)

--- src/main/java/xyz/vikkivuk/chaosmod/procedures/BrownItemInInventoryTickProcedure.java
+++ src/main/java/xyz/vikkivuk/chaosmod/procedures/BrownItemInInventoryTickProcedure.java
@@ -14,13 +14,13 @@ public class BrownItemInInventoryTickProcedure {
 			return;
 		double tick = 0;
 		tick = tick + 1;
-		if (tick == 40) {
-			tick = 0;
+		if (tick >= 40) {
 			if (entity instanceof Player _player) {
 				ItemStack _setstack = new ItemStack(ChaosmodModItems.BROWN.get());
 				_setstack.setCount(1);
 				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
 			}
+			tick = 0;
 		}
 	}
 }
