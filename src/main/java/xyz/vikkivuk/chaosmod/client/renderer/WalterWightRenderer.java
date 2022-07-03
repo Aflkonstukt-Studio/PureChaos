5c5466c060b8dc1c9b05c5d2f6086fdb1d9f0d54
VikkiVuk <viktorblazaric2@gmail.com>
2022-07-03 21:47:12 +0200

    Fixed Tombstone

--- src/main/java/xyz/vikkivuk/chaosmod/client/renderer/WalterWightRenderer.java
+++ src/main/java/xyz/vikkivuk/chaosmod/client/renderer/WalterWightRenderer.java
@@ -1,19 +1,24 @@
 
 package xyz.vikkivuk.chaosmod.client.renderer;
 
-public class WalterWightRenderer extends HumanoidMobRenderer<WalterWightEntity, HumanoidModel<WalterWightEntity>> {
+import xyz.vikkivuk.chaosmod.entity.WalterWightEntity;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
+import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.model.HumanoidModel;
+
+public class WalterWightRenderer extends HumanoidMobRenderer<WalterWightEntity, HumanoidModel<WalterWightEntity>> {
 	public WalterWightRenderer(EntityRendererProvider.Context context) {
 		super(context, new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER)), 0.5f);
-
 		this.addLayer(new HumanoidArmorLayer(this, new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)),
 				new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_OUTER_ARMOR))));
-
 	}
 
 	@Override
 	public ResourceLocation getTextureLocation(WalterWightEntity entity) {
 		return new ResourceLocation("chaosmod:textures/heisenberg-breaking-bad-main.png");
 	}
-
 }
