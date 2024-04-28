a88c47b41c725d97a51e89c14e1ebfbd8244b8a8
JosipTLG <josiph2009@gmail.com>
2024-04-28 23:30:14 +0200

    added recipe for illegal substance
    cartel really cooked with this one

---
+++ src/main/java/xyz/aflkonstukt/purechaos/recipes/brewing/RecipeillegalsubstanceBrewingRecipe.java
@@ -0,0 +1,42 @@
+
+package xyz.aflkonstukt.purechaos.recipes.brewing;
+
+import xyz.aflkonstukt.purechaos.init.PurechaosModPotions;
+import xyz.aflkonstukt.purechaos.init.PurechaosModBlocks;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class RecipeillegalsubstanceBrewingRecipe implements IBrewingRecipe {
+	@SubscribeEvent
+	public static void init(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> BrewingRecipeRegistry.addRecipe(new RecipeillegalsubstanceBrewingRecipe()));
+	}
+
+	@Override
+	public boolean isInput(ItemStack input) {
+		return Ingredient.of(new ItemStack(Items.PAPER)).test(input);
+	}
+
+	@Override
+	public boolean isIngredient(ItemStack ingredient) {
+		return Ingredient.of(new ItemStack(PurechaosModBlocks.GREEN_PLANT.get())).test(ingredient);
+	}
+
+	@Override
+	public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+		if (isInput(input) && isIngredient(ingredient)) {
+			return PotionUtils.setPotion(new ItemStack(Items.POTION), PurechaosModPotions.ILLEGALSUBSTANCE.get());
+		}
+		return ItemStack.EMPTY;
+	}
+}
